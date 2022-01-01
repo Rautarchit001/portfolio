ca5c81dc2edf5e2b5ddfa40f907442577940f6f5
hareesh-r <hareeshprogrammer@gmail.com>
2022-01-01 15:07:16 +0530

    first commit

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -1,40 +1,46 @@
-import React from 'react'
+import { motion } from 'framer-motion'
+import { lazy, Suspense } from 'react'
 import styled, { keyframes, ThemeProvider } from 'styled-components'
-import {DarkTheme} from './Themes';
+
+import { DarkTheme, mediaQueries } from './Themes'
+import astronaut from "../assets/Images/spaceman.png";
+import Loading from '../subComponents/Loading';
+//Components
+const SocialIcons = lazy(() => import('../subComponents/SocialIcons'))
+const PowerButton = lazy(() => import('../subComponents/PowerButton'))
+const LogoComponent = lazy(() => import('../subComponents/LogoComponent'))
+const ParticlesComponent = lazy(() =>
+  import('../subComponents/ParticleComponent')
+)
+const BigTitle = lazy(() => import('../subComponents/BigTitle'))
 
 
-import LogoComponent from '../subComponents/LogoComponent';
-import SocialIcons from '../subComponents/SocialIcons';
-import PowerButton from '../subComponents/PowerButton';
-import ParticleComponent from '../subComponents/ParticleComponent';
-import BigTitle from '../subComponents/BigTitlte'
-import astronaut from '../assets/Images/spaceman.png'
-
-const Box = styled.div`
-background-color: ${props => props.theme.body};
-width: 100vw;
-height:100vh;
-position: relative;
-overflow: hidden;
+const Box = styled(motion.div)`
+  background-color: ${(props) => props.theme.body};
+  width: 100vw;
+  height: 100vh;
+  position: relative;
+  overflow: hidden;
 `
+
 const float = keyframes`
-0% { transform: translateY(-10px) }
-50% { transform: translateY(15px) translateX(15px) }
-100% { transform: translateY(-10px) }
+0% { transform: translateY(-10px)         }
+    50% { transform: translateY(15px) translateX(15px)        }
+    100% { transform: translateY(-10px)         }
+`
 
+const SpaceMan = styled(motion.div)`
+  position: absolute;
+  top: 10%;
+  right: 5%;
+  animation: ${float} 4s ease infinite;
+width:20vw;
+  img{
+    width:100%;
+    height:auto;
+  }
 `
-const Spaceman = styled.div`
-position: absolute;
-top: 10%;
-right: 5%;
-width: 20vw;
-animation: ${float} 4s ease infinite;
-img{
-    width: 100%;
-    height: auto;
-}
-`
-const Main =  styled.div`
+const Main = styled(motion.div)`
   border: 2px solid ${(props) => props.theme.text};
   color: ${(props) => props.theme.text};
   padding: 2rem;
@@ -51,42 +57,74 @@ const Main =  styled.div`
   position: absolute;
   left: calc(5rem + 5vw);
   top: 10rem;
+
   font-family: 'Ubuntu Mono', monospace;
   font-style: italic;
+
+  ${mediaQueries(40)`
+          width: 60vw;
+          height: 50vh;
+          top:50%;
+          left:50%;
+          transform:translate(-50%,-50%);
+
+
+  `};
+  ${mediaQueries(30)`
+          width: 50vw;
+          height: auto;
+          backdrop-filter: none;
+          margin-top:2rem;
+
+  `};
+
+${mediaQueries(20)`
+          padding: 1rem;
+          font-size: calc(0.5rem + 1vw);
+  `};
+
 `
-
-
-
-
 const AboutPage = () => {
-    return (
-        <ThemeProvider theme={DarkTheme}>
-<Box>
+  return (
+    <ThemeProvider theme={DarkTheme}>
+      <Suspense fallback={<Loading/>}>
+        <Box
+          key='skills'
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1, transition: { duration: 0.5 } }}
+          exit={{ opacity: 0, transition: { duration: 0.5 } }}>
+          <LogoComponent theme='dark' />
+          <PowerButton />
+          <SocialIcons theme='dark' />
+          <ParticlesComponent theme='dark' />
 
-<LogoComponent theme='dark'/>
-<SocialIcons theme='dark'/>
-<PowerButton />
-<ParticleComponent theme='dark' />
-
-        <Spaceman>
-            <img src={astronaut} alt="spaceman" />
-        </Spaceman>    
-        <Main>
-        I'm a front-end developer located in India. I love to create simple yet beautiful websites with great user experience.
-<br /> <br/>
-I'm interested in the whole frontend stack Like trying new things and building great projects. I'm an independent freelancer and blogger. I love to write blogs and read books.
-<br/> <br/>
-I believe everything is an Art when you put your consciousness in it. You can connect with me via social links.
-        </Main>
-
-        <BigTitle text="ABOUT" top="10%" left="5%" />
-
-
+          <SpaceMan
+            initial={{ right: '-20%', top: '100%' }}
+            animate={{
+              right: '5%',
+              top: '10%',
+              transition: { duration: 2, delay: 0.5 },
+            }}>
+              <img src={astronaut}  alt="spaceman" />
+          </SpaceMan>
+          <Main
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1, transition: { duration: 1, delay: 1 } }}>
+            I'm a front-end developer located in India. I love to create simple
+            yet beautiful websites with great user experience.
+            <br />
+            <br />
+            I'm interested in the whole frontend stack Like trying new things
+            and building great projects. I'm an independent freelancer and
+            blogger. I love to write blogs and read books. <br />
+            <br />I believe everything is an Art when you put your consciousness
+            in it. You can connect with me via social links.{' '}
+          </Main>
+          <BigTitle text='ABOUT' top='10%' left='5%' />
         </Box>
-
-        </ThemeProvider>
-        
-    )
+      </Suspense>
+    </ThemeProvider>
+  )
 }
 
 export default AboutPage
--- src/components/WorkPage.js
+++ src/components/WorkPage.js
@@ -1,116 +1,153 @@
-import React, { useEffect, useRef } from 'react'
-import styled, { ThemeProvider } from 'styled-components'
-import {DarkTheme} from './Themes';
-import {motion} from 'framer-motion';
+import { motion } from "framer-motion";
+import { useEffect, useRef, lazy, Suspense } from "react";
 
-import LogoComponent from '../subComponents/LogoComponent';
-import SocialIcons from '../subComponents/SocialIcons';
-import PowerButton from '../subComponents/PowerButton';
+import styled, { ThemeProvider } from "styled-components";
 
-import { Work } from "../data/WorkData";
-import Card from '../subComponents/Card';
-import { YinYang } from './AllSvgs';
-import BigTitlte from '../subComponents/BigTitlte';
+import { YinYang } from "./AllSvgs";
+import { Work } from "./WorkData";
+import { DarkTheme, mediaQueries } from "./Themes";
 
-const Box = styled.div`
-background-color: ${props => props.theme.body};
+import Card from "../subComponents/Card";
+import Loading from "../subComponents/Loading";
 
-height:400vh;
-position: relative;
-display: flex;
-align-items: center;
+const SocialIcons = lazy(() => import("../subComponents/SocialIcons"));
+const PowerButton = lazy(() => import("../subComponents/PowerButton"));
+const LogoComponent = lazy(() => import("../subComponents/LogoComponent"));
+const BigTitle = lazy(() => import("../subComponents/BigTitle"));
 
-
-`
+const Box = styled(motion.div)`
+  background-color: ${(props) => props.theme.body};
+  position: relative;
+  display: flex;
+  height: 400vh;
+`;
 
 const Main = styled(motion.ul)`
-position: fixed;
-top: 12rem;
-left:calc(10rem + 15vw);
-height: 40vh;
-display: flex;
+  position: fixed;
+  top: 12rem;
+  left: calc(10rem + 15vw);
 
-color:white;
-`
+  height: 40vh;
+  /* height:200vh; */
+  //border:1px solid white;
+
+  display: flex;
+
+  ${mediaQueries(50)`
+        
+        
+        left:calc(8rem + 15vw);
+
+  `};
+
+  ${mediaQueries(40)`
+  top: 30%;
+        
+        left:calc(6rem + 15vw);
+
+  `};
+
+  ${mediaQueries(40)`
+        
+        left:calc(2rem + 15vw);
+
+  `};
+  ${mediaQueries(25)`
+        
+        left:calc(1rem + 15vw);
+
+  `};
+`;
+
 const Rotate = styled.span`
-display:block;
-position: fixed;
-right:1rem;
-bottom: 1rem;
-width: 80px;
-height: 80px;
-z-index:1;
-`
+  display: block;
+  position: fixed;
+  right: 1rem;
+  bottom: 1rem;
+  width: 80px;
+  height: 80px;
 
+  z-index: 1;
+  ${mediaQueries(40)`
+     width:60px;
+         height:60px;   
+       svg{
+         width:60px;
+         height:60px;
+       }
 
-// Framer-motion Configuration
+  `};
+  ${mediaQueries(25)`
+        width:50px;
+         height:50px;
+        svg{
+         width:50px;
+         height:50px;
+       }
+
+  `};
+`;
 const container = {
-
-  hidden: {opacity:0},
+  hidden: { opacity: 0 },
   show: {
-    opacity:1,
+    opacity: 1,
 
-    transition:{
+    transition: {
       staggerChildren: 0.5,
       duration: 0.5,
-    }
-  }
-
-}
+    },
+  },
+};
 
 const WorkPage = () => {
+  const ref = useRef(null);
 
-    const ref = useRef(null);
-    const yinyang = useRef(null);
+  const yinyang = useRef(null);
 
+  useEffect(() => {
+    let element = ref.current;
 
+    const rotate = () => {
+      element.style.transform = `translateX(${-window.pageYOffset}px)`;
 
-    useEffect(() => {
-        let element = ref.current;
-       
-        
-        const rotate = () => {
-         
-         element.style.transform = `translateX(${-window.pageYOffset}px)`
-      
-         
-          return (yinyang.current.style.transform =
-            'rotate(' + -window.pageYOffset + 'deg)')
-        }
-    
-        window.addEventListener('scroll', rotate)
-        return () => {
-          window.removeEventListener('scroll', rotate);
-          
-        }
-      }, [])
+      return (yinyang.current.style.transform =
+        "rotate(" + -window.pageYOffset + "deg)");
+    };
 
+    window.addEventListener("scroll", rotate);
+    return () => {
+      window.removeEventListener("scroll", rotate);
+    };
+  }, []);
 
-    return (
-        <ThemeProvider theme={DarkTheme}>
-<Box>
+  return (
+    <ThemeProvider theme={DarkTheme}>
+      <Suspense fallback={<Loading />}>
+        <Box
+          key="work"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1, transition: { duration: 1 } }}
+          exit={{ opacity: 0, transition: { duration: 0.5 } }}
+        >
+          <LogoComponent theme="dark" />
+          <PowerButton />
+          <SocialIcons theme="dark" />
 
-<LogoComponent theme='dark'/>
-<SocialIcons theme='dark'/>
-<PowerButton />
+          <Main ref={ref} variants={container} initial="hidden" animate="show">
+            {Work.map((d) => (
+              <Card key={d.id} data={d} />
+            ))}
+          </Main>
 
-     <Main ref={ref}   variants={container} initial='hidden' animate='show'  >
-         {
-            Work.map( d => 
-            <Card key={d.id} data={d} />
-            )
-         }
-     </Main>
-<Rotate ref={yinyang}>
-    <YinYang width={80} height={80} fill={DarkTheme.text} />
-</Rotate>
+          <BigTitle text="WORK" top="10%" right="20%" />
 
-<BigTitlte text="WORK" top='10%' right="20%" />
+          <Rotate ref={yinyang}>
+            <YinYang width={80} height={80} fill={DarkTheme.text} />
+          </Rotate>
         </Box>
+      </Suspense>
+    </ThemeProvider>
+  );
+};
 
-        </ThemeProvider>
-        
-    )
-}
-
-export default WorkPage
+export default WorkPage;
