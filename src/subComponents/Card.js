91ff113b65b5871a151b7f58f6d8006b67f33f91
hareesh-r <hareeshprogrammer@gmail.com>
2022-12-07 21:11:23 +0530

    feat : New animation and colors

--- src/subComponents/Card.js
+++ src/subComponents/Card.js
@@ -19,11 +19,6 @@ const Box = styled(motion.li)`
   border: 1px solid ${(props) => props.theme.body};
   transition: all 0.2s ease;
   cursor: crosshair;
-  &:hover {
-    background-color: ${(props) => props.theme.body};
-    color: ${(props) => props.theme.text};
-    border: 1px solid ${(props) => props.theme.text};
-  }
   ${mediaQueries(50)`
         width:16rem;
         margin-right:6rem;
@@ -63,13 +58,10 @@ const Description = styled.h4`
   `};
 `;
 const Tags = styled.div`
-  border-top: 2px solid ${(props) => props.theme.body};
+  border-top: 2px solid #FCF6F4;
   padding-top: 0.5rem;
   display: flex;
   flex-wrap: wrap;
-  ${Box}:hover & {
-    border-top: 2px solid ${(props) => props.theme.text};
-  }
 `;
 
 const Image = styled.img`
@@ -98,20 +90,9 @@ const Link = styled(NavLink)`
   padding: 0.5rem calc(2rem + 2vw);
   border-radius: 0 0 0 50px;
   font-size: calc(1em + 0.5vw);
-
-  ${Box}:hover & {
-    background-color: ${(props) => props.theme.text};
-    color: ${(props) => props.theme.body};
-  }
 `;
 const Git = styled(NavLink)`
-  color: inherit;
   text-decoration: none;
-
-  ${Box}:hover & {
-    & > * {
-      fill: ${(props) => props.theme.text};
-    }
   }
 `;
 
@@ -133,19 +114,19 @@ const Card = (props) => {
             </Box>
           </div>
           <div class="flip-card-back">
-            <Box key={id} variants={item}>
+            <Box class="box" key={id} variants={item}>
               <Image src={image} />
-              <Tags>
+              <Tags class="box">
                 {tags.map((t, id) => (
                   <Tag key={id}>#{t}</Tag>
                 ))}
               </Tags>
               <Footer>
-                <Link to={{ pathname: `${demo}` }} target="_blank">
+                <Link class="visit-link" to={{ pathname: `${demo}` }} target="_blank">
                   Visit
                 </Link>
                 <Git to={{ pathname: `${github}` }} target="_blank">
-                  <Github width={30} height={30} />
+                  <Github class="git" width={30} height={30} />
                 </Git>
               </Footer>
             </Box>
